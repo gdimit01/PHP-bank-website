ed6c48deac65ea647d8ab984eab5bba56810c7d6
Georgi Dimitrov <91046490+gdimit01@users.noreply.github.com>
2023-09-09 13:00:10 +0100

    updated dbconnect to work with phpMyAdmin

--- dbconnect.php
+++ dbconnect.php
@@ -1,19 +1,22 @@
 <?php 
 function connect() { 
-    $host = 'dragon.ukc.ac.uk'; 
-    $dbname = 'gd353'; 
-    $user = 'gd353'; 
-    $pwd = 'o2ormus';
+    $host = 'localhost'; 
+    $dbname = 'db_bank'; 
+    $user = 'php'; 
+    $pwd = '1980';
 
-    $dsn = "mysql:host=$host;dbname=$dbname";
+    $dsn = "mysql:host=$host;dbname=$dbname;charset=utf8mb4"; // Added charset for better character support
     $options = [
-        PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION
+        PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+        PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC, // Set default fetch mode to associative array
+        PDO::ATTR_EMULATE_PREPARES => false, // Use real prepared statements
     ];
+
     try { 
-        $conn = new PDO($dsn, $user, $pwd, $options); // use $dsn instead of the connection string
+        $conn = new PDO($dsn, $user, $pwd, $options);
         return $conn; 
     } catch (PDOException $e) { 
-        echo "PDOException: ".$e->getMessage(); 
+        error_log("PDOException: " . $e->getMessage()); // Log the error instead of echoing it
         return null;
     } 
 } 
